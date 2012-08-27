b85282445818548207dd355ab3807e126e295f1d
Anton Tolchanov <me@knyar.net>
2012-08-27 05:55:00 +0000

    flickr search adjusted, image fetch URL is now in background

--- kittens/kittens.py
+++ kittens/kittens.py
@@ -45,12 +45,13 @@ def fetch_new_image():
     flickr = flickrapi.FlickrAPI(settings.FLICKR_API_KEY)
     photos = flickr.photos_search(
             license='1,2,4,5', # attribution
+            sort='interestingness-desc',
             content_type='1', # photos
             group_id='10917369@N00', # http://www.flickr.com/groups/cat-portraits/
             media='photos',
             extras='original_format',
             per_page='30',
-            page = random.randint(1, 296),
+            page = random.randint(1, 150),
             )
     if photos.attrib['stat'] != 'ok': return False
     for i in xrange(30):
